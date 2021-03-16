5fb42571cdc7b7771cd7a6eb7e828d663e39de7c
moslevy <moses.levy@gmail.com>
2021-03-16 11:17:00 -0300

    Added CustomExceptionA.java

--- src/main/java/com/example/hw3/townhallinterfaces/exceptions/CustomExceptionA.java
+++ src/main/java/com/example/hw3/townhallinterfaces/exceptions/CustomExceptionA.java
@@ -1,6 +1,6 @@
 package com.example.hw3.townhallinterfaces.exceptions;
 
-public class CustomExceptionA extends Exception{
+public class CustomExceptionA extends RuntimeException{
     public CustomExceptionA(String message){
         super(message);
     }
